62bb5ddfbfafe8ac1a53eb9d149f37d01e3535ce
SanaIshaqat <sanaishaqat@gmail.com>
2022-02-05 01:14:40 +0200

    Updates

---
+++ precache-manifest.531b5f57813f6f7de9051c3add7bae5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ee5de714c713be50c69c51e6ea3e5a",
+    "url": "/moviesApp/index.html"
+  },
+  {
+    "revision": "d05fcb18ee6337e641ec",
+    "url": "/moviesApp/static/css/main.ff3d3356.chunk.css"
+  },
+  {
+    "revision": "cfccd96e735fa8b24252",
+    "url": "/moviesApp/static/js/2.2822f613.chunk.js"
+  },
+  {
+    "revision": "d05fcb18ee6337e641ec",
+    "url": "/moviesApp/static/js/main.ced0d2d0.chunk.js"
+  },
+  {
+    "revision": "a69d6f3df8b67a165393",
+    "url": "/moviesApp/static/js/runtime-main.5fe44799.js"
+  },
+  {
+    "revision": "64f4bb9e2dde81a10899dd0467d45ad4",
+    "url": "/moviesApp/static/media/LOGOmovies.64f4bb9e.png"
+  }
+]);
